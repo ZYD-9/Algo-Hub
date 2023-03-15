4cb7ad866731c8b9f8ff7e5906555d6e344be204
Novem <novemjanydzel@gmail.com>
2023-03-15 23:54:16 +0000

    2nd commit

--- Testsound.java
+++ Testsound.java
@@ -1,27 +1,3 @@
 import javax.sound.midi.*;
 import javax.sound.midi.spi.*;
 
-public class Testsound {
-
-    public void play()
-    {
-        try {
-            Sequencer player = new MidiSystem.getSequencer();
-            player.open();
-        }
-        finally{
-            play();
-
-        }
-
-    }
-    
-    
-    public static void main(String args[])
-    {
-     Testsound music = new Testsound();
-     music.play();
-        
-    }
-    
-}
